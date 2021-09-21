251aa0b0f4883a3aee6d15d3b9cb2dd303d5dffd
Tejaswini_Sangana <Tejaswini_Sangana@epam.com>
2021-09-21 12:37:48 +0530

    committed

--- src/main/java/com/weatherapi/reusableclasses/WeatherInformation.java
+++ src/main/java/com/weatherapi/reusableclasses/WeatherInformation.java
@@ -1,23 +1,23 @@
 package com.weatherapi.reusableclasses;
 
-import com.weatherapi.utility.LogUtility;
 import io.restassured.response.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
+import org.apache.log4j.*;
 
 public class WeatherInformation {
     private WeatherInformation(){}
+    public static final Logger logger = Logger.getLogger(WeatherInformation.class);
     public static void getWeatherData(Response response){
         JSONObject jsonObject= new JSONObject(response.asString());
         JSONArray weatherData=(JSONArray) jsonObject.get("data");
         JSONObject data= (JSONObject) weatherData.get(0);
-        LogUtility.logInfo("Date and time:"+data.get("datetime"));
-        LogUtility.logInfo("Country_Code:"+data.get("country_code"));
-        LogUtility.logInfo("City_Name:"+data.get("city_name"));
+        logger.info("Date and time:"+data.get("datetime"));
+        logger.info("Country_Code:"+data.get("country_code"));
+        logger.info("City_Name:"+data.get("city_name"));
         JSONObject weatherInfo=data.getJSONObject("weather");
-        LogUtility.logInfo("Weather code:"+weatherInfo.get("code"));
-        LogUtility.logInfo("Weather Description:"+weatherInfo.get("description"));
+        logger.info("Weather code:"+weatherInfo.get("code"));
+        logger.info("Weather Description:"+weatherInfo.get("description"));
         System.out.println();
     }
 }
